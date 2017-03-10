d443c309f5b4a18b848de81caac64599244147f8
Pablo <pablodelatorree@gmail.com>
2017-03-10 12:24:48 +0100

    Ejecución en MAIN

--- ProyectoBus/ProyectoBus.java
+++ ProyectoBus/ProyectoBus.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package proyectobus;
+import interfaz.InterfazTicketera;
 
 /**
  *
@@ -15,7 +16,9 @@ public class ProyectoBus {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+         java.awt.EventQueue.invokeLater(() -> {
+            new InterfazTicketera().setVisible(true);
+        });
     }
     
 }
