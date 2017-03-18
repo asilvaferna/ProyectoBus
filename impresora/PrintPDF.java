a35ce2923be395e183f0978cc4ec87acaeedcf96
adri4silva <adri4silva@gmail.com>
2017-03-18 13:33:26 +0100

    Modificaciones para generar PDF sin errores

--- impresora/PrintPDF.java
+++ impresora/PrintPDF.java
@@ -4,69 +4,66 @@
  * and open the template in the editor.
  */
 package impresora;
+
 import java.io.IOException;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import interfaz.Ticket;
+
 /**
  *
  * @author Adri
  */
 public class PrintPDF {
-        public void imprimePagina(){
-//            try{
-//        
-//        System.out.println("Create Simple PDF file with Text");
-//        String fileName = "PdfWithtext.pdf"; // name of our file
-//        
-//        PDDocument doc = new PDDocument();
-//        PDPage page = new PDPage();
-//
-//        doc.addPage(page);
-//
-//        PDPageContentStream content = new PDPageContentStream(doc, page);
-//        
-//        content.beginText();
-//        content.setFont(PDType1Font.HELVETICA, 26);
-//        content.moveTo(220, 750);
-//        content.drawString("Registration Form");
-//        content.endText();
-//        
-//        
-//        content.beginText();
-//        content.setFont(PDType1Font.HELVETICA, 16);
-//        content.moveTo(80, 700);
-//        content.drawString("Name : ");
-//        content.endText();
-//        
-//        
-//        content.beginText();
-//        content.setFont(PDType1Font.HELVETICA, 16);
-//        content.moveTo(80,650);
-//        content.drawString("Father Name : ");
-//        content.endText();
-//        
-//        content.beginText();
-//        content.setFont(PDType1Font.HELVETICA, 16);
-//        content.moveTo(80,600);
-//        content.showText("Hola");
-//        content.endText();
-//        
-//        
-//        content.close();
-//        doc.save(fileName);
-//        doc.close();
-//        
-//        System.out.println("your file created in : "+ System.getProperty("user.dir"));
-//
-//        }
-//        catch(IOException e){
-//        
-//        System.out.println(e.getMessage());
-//        
-//        }
+
+    public void imprimePagina() {
+        try {
+            Ticket ticket = new Ticket();
+            
+            String fileName = "ticket.pdf";
+
+            PDDocument doc = new PDDocument();
+            PDPage page = new PDPage();
+
+            doc.addPage(page);
+
+            PDPageContentStream content = new PDPageContentStream(doc, page);
+
+            content.beginText();
+            content.setFont(PDType1Font.HELVETICA, 26);
+            content.newLineAtOffset(220, 750);
+            content.showText("Ticket");
+            content.endText();
+
+            content.beginText();
+            content.setFont(PDType1Font.HELVETICA, 16);
+            content.newLineAtOffset(80, 700);
+            content.showText("Name : ");
+            content.endText();
+
+            content.beginText();
+            content.setFont(PDType1Font.HELVETICA, 16);
+            content.newLineAtOffset(80, 650);
+            content.showText("Father Name : ");
+            content.endText();
+
+            content.beginText();
+            content.setFont(PDType1Font.HELVETICA, 16);
+            content.newLineAtOffset(80, 600);
+            content.showText("Hola");
+            content.endText();
+
+            content.close();
+            doc.save(fileName);
+            doc.close();
+
+        } catch (IOException e) {
+
+            System.out.println(e.getMessage());
 
         }
-    }
 
+    }
+}
